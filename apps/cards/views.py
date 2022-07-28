8917c8eff2a1b114da70f7344efaf34d78a45b03
hengage <tunecarterkillz@gmail.com>
2022-07-28 17:36:51 +0100

    Feature: created templates and base templates

--- apps/cards/views.py
+++ apps/cards/views.py
@@ -2,10 +2,7 @@ from django.views.generic import TemplateView
 from django.http import HttpResponse
 import datetime
 
-def HomePageView(request):
-    now = datetime.datetime.now()
-    html = "<html><body>It is now %s.</body></html>" % now
-    return HttpResponse(html)
 
-# class HomePageView(TemplateView):
-#     template_name = 'home.html'
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
