a7688feaca0281d1a83019d719b073c067ea3224
hengage <tunecarterkillz@gmail.com>
2022-07-28 18:52:15 +0100

    Feature: created card models

--- apps/cards/models.py
+++ apps/cards/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+NUM_BOXES = 5
+BOXES = range(1, NUM_BOXES + 1)
+
+class CardModel(models.Model):
+    question = models.CharField(max_length=150)
+    answer = models.CharField(max_length=150)
+    box = models.IntegerField(choices=zip(BOXES, BOXES), default=BOXES[0])
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.question
+
+    class Meta:
+        ordering = ['-date_created']
+        verbose_name = 'card'
+        verbose_name_plural = 'cards'
