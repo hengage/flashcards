6df8b62248dafc2b1809a5baccd1e4388bee4d84
hengage <tunecarterkillz@gmail.com>
2022-07-29 14:06:27 +0100

    Feature: output the card list on homepage

--- apps/cards/views.py
+++ apps/cards/views.py
@@ -1,8 +1,10 @@
-from django.views.generic import TemplateView
-from django.http import HttpResponse
-import datetime
+from django.views.generic import TemplateView, ListView
+from .models import CardModel
 
 
 
-class HomePageView(TemplateView):
+class HomePageView( ListView):
     template_name = 'home.html'
+    model = CardModel
+    queryset = CardModel.objects.all()
+    context_object_name = 'card_list'
