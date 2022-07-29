5520f36d2d4ebeaad3a81800d4bdb0d432853958
hengage <tunecarterkillz@gmail.com>
2022-07-29 20:13:44 +0100

    Feature: edit card faeture

--- apps/cards/urls.py
+++ apps/cards/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import HomePageView, CreateCardView
+from .views import HomePageView, CreateCardView, UpdatecardView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
-    path('new', CreateCardView.as_view(), name='create-card')
+    path('new', CreateCardView.as_view(), name='create-card'),
+    path('edit/<int:pk>', UpdatecardView.as_view(), 'update-card')
 ]
--- apps/cards/views.py
+++ apps/cards/views.py
@@ -1,5 +1,7 @@
 from django.urls import reverse_lazy
-from django.views.generic import TemplateView, ListView, CreateView
+from django.views.generic import (
+TemplateView, ListView, CreateView, UpdateView
+)
 from .models import CardModel
 
 
@@ -14,4 +16,7 @@ class CreateCardView(CreateView):
     model = CardModel
     template_name = 'cards/create_card.html'
     fields = ['question', 'answer', 'box']
+    success_url = reverse_lazy('home')
+
+class UpdatecardView(CreateCardView, UpdateView):
     success_url = reverse_lazy('home')
