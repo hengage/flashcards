ae6262bd39d41ac8eaecb858f3a68cb025a294e5
hengage <tunecarterkillz@gmail.com>
2022-08-02 13:58:01 +0100

    Refactor: changed the templates setting

--- flashcards/settings.py
+++ flashcards/settings.py
@@ -47,7 +47,7 @@ ROOT_URLCONF = 'flashcards.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
