efb0aee9620a9fe526e879805d1b739a7efb42e2
hengage <tunecarterkillz@gmail.com>
2022-08-03 02:00:37 +0100

    Feature: registered custom user model in admin

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import CustomUserModel
+
+@admin.register(CustomUserModel)
+class CustomUserAdmin(UserAdmin):
+    list_display = ['username', 'email', 'last_login']
